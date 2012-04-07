6a16d5ee3df204322d22d9a56d6b872927a849e3
fabiooshiro <fabio.oshiro@gmail.com>
2012-04-07 01:43:47 -0300

    more idea?

--- handlebars-backbone.js
+++ handlebars-backbone.js
@@ -18,25 +18,77 @@
 	function unwrapFunctions(obj){
 		var res = {};
 		for(var p in obj){
-			if(typeof(obj[p]) == 'function'){
+			var tp = typeof(obj[p]); 
+			if(tp == 'function'){
 				res[p] = obj[p]();
+			}else if(tp == 'object'){
+				var un = obj[p]; 
+				if(obj[p] && obj[p]['toJSON']){
+					un = obj[p]['toJSON']();
+				}
+				res[p] = unwrapFunctions(un);
 			}else{
 				res[p] = obj[p];
 			}
 		}
 		return res;
 	}
+
+	var htmlesc = Handlebars.Utils.escapeExpression;
+	var widgets = {
+		select: function(name, val, opts){
+			var out = $('<select>').attr('name', name);
+			for(var i = 0; i < opts.source.length; i ++){
+				var item = opts.source[i];
+				out.append($('<option>').attr('value', item.get('id')).text(item.get('name')));
+			}
+			return $('<div>').append(out).html();
+		}
+	};
 	
-	function bind2way(name, jel, model){
+	var handleWidget;
+	Handlebars.registerHelper('handleWidget', function(name, v2) {
+		var opts = handleWidget[name];
+		var obj = this[name];
+		if(opts){
+			var html = widgets[opts.widget](name, obj, opts);
+			var path = name.split('\.');
+			var co = obj;
+			var lp;
+			for(var i = 0; i < path.length; i++){
+				lp = path[i];
+				if(i < path.length - 1)
+					co = co[lp];
+			}
+			return new Handlebars.SafeString(html);
+		}
+		return "define " + name;
+	});
+	
+	function bind2way(name, jel, model, handleWidget){
 		jel.find('[name="' + name + '"]').each(function(i, el){
 			var _jel = $(el);
 			var val = model.get(name);
 			if(val){
-				_jel.val(val);
-				_jel.change(function(){
-					model.set(name, _jel.val());
-				});
+				if(val && typeof(val) == 'object' && val['id']){
+					_jel.val(val.id);
+				}else{
+					_jel.val(val);	
+				}
 			}
+			_jel.change(function(){
+				var val = _jel.val();
+				if(handleWidget && handleWidget[name]){
+					for(var i = 0; i < handleWidget[name].source.length; i++){
+						if(handleWidget[name].source[i].id == val){
+							val = handleWidget[name].source[i];	
+							break;
+						}
+					} 
+				}
+				//console.log("set " + name + " = " + val);
+				model.set(name, val);
+			});
 		});	
 	}
 	
@@ -51,11 +103,12 @@
 				self.compiledTemplate = Handlebars.compile(source);	
 			}
 			var jsonModel = unwrapFunctions(model.toJSON());
+			handleWidget = self.handleWidget;
 			var jel = $(this.el); 
 			jel.html(self.compiledTemplate(jsonModel));
 			
 			for(name in jsonModel){
-				bind2way(name, jel, model);
+				bind2way(name, jel, model, self.handleWidget);
 			}
 		}
 	});	
